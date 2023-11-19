7dbb1c9e6cd3a8886218250f7b45559ae81687e1
Jesus Lopez <jebus.lpz@gmail.com>
2023-11-19 12:07:32 -0700

    Authenticates user with URL

--- main.go
+++ main.go
@@ -1,24 +1,83 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"io/ioutil"
+	"github.com/zmb3/spotify/v2"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+	"log"
 	"net/http"
+	"time"
+)
+
+const redirectURI = "http://localhost:8080/callback"
+
+var (
+	auth = spotifyauth.New(
+		spotifyauth.WithRedirectURL(redirectURI),
+		spotifyauth.WithScopes(spotifyauth.ScopeUserReadPrivate,
+			spotifyauth.ScopeUserReadCurrentlyPlaying,
+			spotifyauth.ScopeUserReadPlaybackState,
+			spotifyauth.ScopeUserModifyPlaybackState,
+			spotifyauth.ScopeStreaming),
+		spotifyauth.WithClientID("181c70216d5f4d3d9fafc311a839b506"),
+		spotifyauth.WithClientSecret("8c83d8c2d79e494ab747563131cd4c4f"),
+	)
+	ch    = make(chan *spotify.Client)
+	state = "abc123"
+	s     = &http.Server{
+		Addr:           ":8080",
+		Handler:        nil,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
 )
 
 func main() {
-	resp, err := http.Get("https://www.example.com/")
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
+	http.HandleFunc("/callback", completeAuth)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Got request for:", r.URL.String())
+	})
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 
-	fmt.Println(string(body))
+	url := auth.AuthURL(state)
+	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
+
+	// wait for auth to complete
+	client := <-ch
+
+	// use the client to make calls that require authorization
+	user, err := client.CurrentUser(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("You are logged in as:", user.ID)
+
 }
+
+func completeAuth(w http.ResponseWriter, r *http.Request) {
+	tok, err := auth.Token(r.Context(), state, r)
+	if err != nil {
+		http.Error(w, "Couldn't get token", http.StatusForbidden)
+		log.Fatal(err)
+	}
+	if st := r.FormValue("state"); st != state {
+		http.NotFound(w, r)
+		log.Fatalf("State mismatch: %s != %s\n", st, state)
+	}
+
+	// use the token to get an authenticated client
+	client := spotify.New(auth.Client(r.Context(), tok))
+	fmt.Fprintf(w, "Login Completed!")
+	ch <- client
+}
+
+// https://open.spotify.com/artist/2wY79sveU1sp5g7SokKOiI?si=muSGAz1hQPi0VcpUFS8ihw
+// https://open.spotify.com/artist/3nFB4eMP5gdqee2eQb8nZb?si=KTMSCYT7Qfq_ErR1jwpyrQ
